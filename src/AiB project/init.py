49817a3a4938f83be90f1ba35d726b098d136d61
InfiniteLobster <szcholewinski@gmail.com>
2026-06-19 15:54:09 +0200

    Additional comments

--- src/AiB project/init.py
+++ src/AiB project/init.py
@@ -2,19 +2,19 @@
 import numpy as np
 #--------------------Functions--------------------#
 #By design each function is for one weight layer initialization
-#
+#Initialization of weights and biases as zeros
 def init_zero(shape: np.ndarray, dtype: type) -> np.ndarray:
-    #
+    #initializing weight matrix (with zeros as it is zero initialization) with given shape and data type
     weights = np.zeros(shape, dtype)
-    #
+    #returning output
     return weights
-#
+#Xavier/Glorot uniform initialization of weights
 def init_xavier_uniform(shape: np.ndarray, dtype: type) -> np.ndarray:
-    #
+    #calculating fan_in and fan_out for Xavier/Glorot initialization
     fan_in = shape[1]
     fan_out = shape[0]
     # Xavier/Glorot uniform initialization
     limit = np.sqrt(6 / (fan_in + fan_out))
     weights = np.random.uniform(-limit, limit, size=shape).astype(dtype)
-    #
+    #returning output
     return weights
